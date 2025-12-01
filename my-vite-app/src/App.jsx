af616d39551916b6c9b873c6ee7f7973fd145a7c
poojajambukiya3-bit <poojajambukiya3@gmail.com>
2025-12-01 16:07:06 +0530

    clone

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'; 
+import Create from './Component/Create.jsx';
+
+const App = () => {
+  return (
+    <div className='container'>
+     
+      <Create/>
+    </div> 
+  )
+}
+
+export default App
---
+++ my-vite-app/src/Component/Create.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <>
+      <h2>CREATE</h2>
+       <form>
+  
+<div className="mb-3">
+    <label  className="form-label">Name</label>
+    <input type="text" className="form-control" />
+   </div>
+
+<div classname="mb-3">
+    <label className="form-label">Email address</label>
+    <input type="email" className="form-control"  aria-describedby="emailHelp"/>
+    </div>
+  <div>
+  <button type="submit" className="btn btn-primary mt-3">Submit</button>
+  </div>
+</form>
+    </>
+  )
+}
+
+export default Create
