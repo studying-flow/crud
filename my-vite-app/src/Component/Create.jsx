df98dc4667cd8d197bdc59017fbdce6f380c1841
poojajambukiya3-bit <poojajambukiya3@gmail.com>
2025-12-02 11:38:32 +0530

    updated Create.jsx

--- my-vite-app/src/Component/Create.jsx
+++ my-vite-app/src/Component/Create.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 
 const Create = () => {
+const[name,setName] = useState("")
+const[email,setEmail] = useState("")
 
+const header = {"Access-Control-Allow-Origin":"*"};
+
+const handleSubmit = () =>{
+  console.log("clclekd");
   
+  axios.post(
+    "https://692d3c47e5f67cd80a4aa93c.mockapi.io/api/crud/crud-youtube",{
+       name:name,
+       email:email,
+       header,
+    });
+};
+
   return (
     <>
       <h2>CREATE</h2>
@@ -10,16 +25,17 @@ const Create = () => {
   
 <div className="mb-3">
     <label  className="form-label">Name</label>
-    <input type="text" className="form-control" />
+    <input type="text" className="form-control"
+    onChange={(e)=>setName(e.target.value)} />
    </div>
 
-<div classname="mb-3">
+<div className="mb-3">
     <label className="form-label">Email address</label>
-    <input type="email" className="form-control"  aria-describedby="emailHelp"/>
+    <input type="email" className="form-control"  aria-describedby="emailHelp"
+    onChange={(e)=>setEmail(e.target.value)}/>
     </div>
-  <div>
-  <button type="submit" className="btn btn-primary mt-3 ">Submit</button>
-  </div>
+
+  <button type="submit" className="btn btn-primary mt-3 " onClick={handleSubmit}>Submit</button>
 </form>
     </>
   )
